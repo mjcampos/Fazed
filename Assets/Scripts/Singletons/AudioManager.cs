dcd83165a8793b28ffc8b913aadb36f03970c518
Michael Campos <mjcampos@users.noreply.github.com>
2025-09-28 09:58:05 +0200

    Added AudioManager

---
+++ Assets/Scripts/Singletons/AudioManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    void Awake()
+    {
+        // Singleton setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
